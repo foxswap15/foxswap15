53c78286f865f8b3b369c693ace46872b3bbba3d
PHAM ANH TUAN <tuanpham206i@gmail.com>
2020-09-15 06:43:42 +0700

    FS

---
+++ resources/js/script.js
@@ -0,0 +1,8 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 700) {
+        $("nav").addClass("sticky");
+    } else {
+        $("nav").removeClass("sticky")
+    }
+})
