bd004df5212852a8bccd01273041e60dd473911d
PHAM ANH TUAN <tuanpham206i@gmail.com>
2020-09-15 15:15:46 +0700

    responsive nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,24 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if(scroll >= 700) {
-        $("nav").addClass("sticky");
-    } else {
-        $("nav").removeClass("sticky")
-    }
-})
+
+$( document ).ready(function() {
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200); 
+    })
+
+
+    $('.js--scoll').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+    
+
+        
+
+
+});
